c7afaea7d14ef1deaeba4c309aaaeed99accf117
cadenzah <cadenzah93@gmail.com>
2019-06-11 23:28:22 +0900

    v.0.1.2 - added install procedure - added default values for environment variables

--- README.md
+++ README.md
@@ -8,6 +8,34 @@ MQTT Broker Server for SportsManager_Device, SportsManager_Server or any other M
 
 ## Tech Stack
 
-- ['mosca.js']()
+- ['mosca.js'](https://github.com/mcollina/mosca)
+
+## Prerequisite
+
+- MongoDB Server Instance (For data store)
 
 ## How to use
+
+1. Run `mongod`
+```bash
+$ sudo mongod
+```
+2. Download the project
+```bash
+$ git clone https://github.com/cadenzah/SportsManager_Broker.git
+```
+3. Locate into project's folder
+4. Install npm modules
+```bash
+$ cd SportsManager_Broker
+$ npm install
+```
+5. Make `.env` file. This file includes environment values used in the server. Those values include:
+> - PORT: The port number for the broker server (default value is `1883`)
+> - DB_IP: IP address where MongoDB is running (default value is `localhost`)
+> - DB_PORT: The port number for the DB (default value is `27017`)
+
+6. Run the broker server
+```bash
+$ npm run start
+```
--- app.js
+++ app.js
@@ -1,36 +1,41 @@
 const mosca = require('mosca')
 require('dotenv').config()
 
+const port = process.env.PORT || 1883
+const dbIP = process.env.DB_IP || 'localhost'
+const dbPort = process.env.DB_PORT || 27017
+
 const pubsubSettings = {
   type: 'mongo',
-  url: `mongodb://${process.env.DB_IP}:${process.env.DB_PORT}/sports-manager`,
+  url: `mongodb://${dbIP}:${dbPort}/sports-manager`,
   pubsubCollection: 'ascoltatori',
   mongo: {}
 }
 
 const moscaSettings = {
-  port: parseInt(process.env.PORT),
+  port: parseInt(port),
   backend: pubsubSettings
 }
 
 const server = new mosca.Server(moscaSettings)
 server.on('ready', () => {
-  console.log(`${process.env.PORT} 포트에서 실행중...`)
+  console.log(`${port} 포트에서 실행중...`)
 
   server.on('clientConnected', (client) => {
     console.log(`Client connected: ${client.id}`)
   })
 
   server.on('published', (packet, client) => {
-    console.log(`Published: ${packet.payload}`)
+    console.log(packet)
   })
 
-  server.publish({
-    topic: '/hello',
-    payload: 'Hello!',
-    qos: 0,
-    retain: false
-  }, () => {
-    console.log(`message was sent!`)
-  })
+  // Example code to generate publish myself
+  // server.publish({
+  //   topic: '/hello',
+  //   payload: 'Hello!',
+  //   qos: 0,
+  //   retain: false
+  // }, () => {
+  //   console.log(`message was sent!`)
+  // })
 })
